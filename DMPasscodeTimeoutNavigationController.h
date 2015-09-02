c0fed21c35815a073168e6f1e64efae56376c3dc
minsoe <mszhawk@gmail.com>
2015-09-02 17:19:45 +0800

    add file
    add self dismissing view controller

---
+++ DMPasscodeTimeoutNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  DMPasscodeTimeoutNavigationController.h
+//  Pods
+//
+//  Created by Min Soe Zan on 2/9/15.
+//
+//
+
+#import "DMPasscodeInternalNavigationController.h"
+
+@interface DMPasscodeTimeoutNavigationController : DMPasscodeInternalNavigationController
+
+@end
---
+++ DMPasscodeTimeoutNavigationController.m
@@ -0,0 +1,65 @@
+//
+//  DMPasscodeTimeoutNavigationController.m
+//  Pods
+//
+//  Created by Min Soe Zan on 2/9/15.
+//
+//
+
+#import "DMPasscodeTimeoutNavigationController.h"
+
+@interface DMPasscodeTimeoutNavigationController ()
+
+@property(nonatomic, strong) NSTimer *timer;
+
+@end
+
+@implementation DMPasscodeTimeoutNavigationController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little
+preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+  [super touchesEnded:touches withEvent:event];
+  [self resetTimer];
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+  [super touchesCancelled:touches withEvent:event];
+  [self resetTimer];
+}
+
+- (void)resetTimer {
+  [self.timer invalidate];
+  self.timer = nil;
+  self.timer = [NSTimer timerWithTimeInterval:3
+                                       target:self
+                                     selector:@selector(dismiss)
+                                     userInfo:nil
+                                      repeats:NO];
+  [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+}
+
+- (void)dismiss {
+  [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
